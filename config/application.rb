859d9f5104d2992ebdca8339a45bab5b6d3af0de
James Jefferies <james@shedcode.co.uk>
2020-05-27 10:05:57 +0100

    Correct defaults

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module DroneDataApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.load_defaults 6.0
+    config.load_defaults 5.2
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
